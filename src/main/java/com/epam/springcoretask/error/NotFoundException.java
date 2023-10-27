a4e04db2756a6b26ba84252190417c47e5a3ebb0
Sergey <shikofreid@yandex.ru>
2023-10-27 17:02:55 +0400

    Created branch for PR

--- src/main/java/com/epam/springcoretask/error/NotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.epam.springcoretask.error;
-
-public class NotFoundException extends RuntimeException {
-    public NotFoundException(String message) {
-        super(message);
-    }
-}
